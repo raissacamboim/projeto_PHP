b637abf866aba9c855bb758521852f7229a3c4d0
raissacamboim <raissadasilvacamboim@gmail.com>
2024-11-05 19:57:18 -0300

    projetoPHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Projetos/Exercícios</title>
+</head>
+<body>
+    <header>
+        <h3>PROJETOS</h3> <h3>EXERCÍCIOS DE FIXAÇÃO</h3><h3>2024/2</h3>
+    </header>
+    <div class="container">
+        <a href="./exer1.php">Exercício 1</a>
+        <a href="./#">Exercício 1</a>
+        <a href="./#">Exercício 1</a>
+    </div>
+</body>
+</html>
